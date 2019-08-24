3345a9dc902a2ccf396948fa59c4d1abe08f0d4a
=DamjanRadev <radevdamjan@gmail.com>
2019-08-24 20:34:49 +0200

    Styles for restaurants and foodList, rearange shop page

--- src/components/Restaurants/Restaurant/Restaurant.js
+++ src/components/Restaurants/Restaurant/Restaurant.js
@@ -5,8 +5,13 @@ const restaurant = props => {
   return (
     <li className={classes.Restaurant} onClick={props.clicked}>
       <img src={props.img} alt="rest_logo" />
-      <h3>{props.name}</h3>
-      <p>{props.workTime}</p>
+      <div>
+        <h3>{props.name}</h3>
+        <p>Status: Open</p>
+      </div>
+      <div>
+        <p>{props.description}</p>
+      </div>
     </li>
   );
 };
--- src/containers/Shop/Shop.js
+++ src/containers/Shop/Shop.js
@@ -3,10 +3,18 @@ import { Link } from "react-router-dom";
 import classes from "./Shop.module.css";
 import Restaurants from "./Restaurants/Restaurants";
 import Modal from "../../components/UI/Modal/Modal";
+import FoodList from "../../components/Restaurants/Food/FoodList";
+import axios from "../../apis/shopBackend";
 
 class Shop extends Component {
+  componentDidMount() {
+    axios.get("/shop/foods").then(res => {
+      this.setState({ foods: res.data });
+    });
+  }
   state = {
-    showCart: true
+    showCart: true,
+    foods: []
   };
 
   cartOpenedHandler = () => {
@@ -25,8 +33,10 @@ class Shop extends Component {
           <h3>Restaurants page</h3>
           <Link to={"/create-restaurant"}>Create a new restaurant</Link>
         </div>
-
-        <Restaurants />
+        <div className={classes.ShopContent}>
+          <Restaurants />
+          <div>Dame</div>
+        </div>
       </div>
     );
   }
